a108f9df1bf5fccc30449f73a11942a32a3505fe
Brian Cairl <bc3618@zebra.com>
2022-02-25 23:53:30 +0000

    Match `cmake` build `googletest` version with `bazel`
    This version is stable, and master isn't always...

--- cmake/third_party/googletest.cmake
+++ cmake/third_party/googletest.cmake
@@ -5,7 +5,7 @@ project(googletest-download NONE)
 include(ExternalProject)
 ExternalProject_Add(googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
-  GIT_TAG           master
+  GIT_TAG           c9ccac7cb7345901884aabf5d1a786cfa6e2f397
   SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
   BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
   CONFIGURE_COMMAND ""
